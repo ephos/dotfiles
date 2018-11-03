ddd4b86ccd1ead6edf50c2b5e5384fb0fee9b8df
Rob Pleau (ephos) <rjpleau@gmail.com>
2018-11-02 20:26:45 -0400

    Updated dotfiles

---
+++ scripts/workstation/VSCode/VideoRecordingSettings.md
@@ -0,0 +1,11 @@
+{
+    "workbench.colorTheme": "PowerShell ISE",
+    "workbench.iconTheme": "vscode-icons",
+    "window.zoomLevel": 0,
+    "editor.fontSize": 14,
+    "editor.minimap.enabled": true,
+    "editor.formatOnType": true,
+    "editor.formatOnPaste": true,
+    "editor.folding": true,
+    "editor.wordWrap": "on"
+}
---
+++ scripts/workstation/Windows/Windows_Setup.ps1
@@ -0,0 +1,33 @@
+function Test-Administrator
+{
+    $currentPrincipal = New-Object -TypeName Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)    
+}
+
+
+Write-Output -InputObject 'Setting PSGallery PSRepository to trusted...'
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Write-Output -InputObject 'Updating/Installing modules...'
+Install-Module PowershellGet -Scope CurrentUser -Force
+Install-Module posh-git, PesterVideoRecordingSettings
+
+Write-Output -InputObject 'Updating help files...'
+Update-Help -Force
+
+switch ($PSVersionTable.Platform)
+{
+    'Windows'
+    {
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+    }
+    'Unix'
+    {
+        Set-PSReadLineOption -EditMode Windows
+    }
+    default
+    {
+        Write-Error -Message "Script does not contain options for $($PSVersionTable.Platform)." -ErrorAction Stop
+    }
+}
+
