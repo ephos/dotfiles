196e3ae3999afd2d4f19c8aa0b10c8ecd21d6029
Rob <rjpleau@gmail.com>
2020-11-15 17:04:39 -0500

    Added New Config Files

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -19,62 +19,47 @@ else {
 
 #region PROMPT SETUP
 ############################
-
-# Characters / Keys
-# Fun random char decorator (https://unicode.org/emoji/charts/full-emoji-list.html)
-function Get-RandomPromptChar {
-    $decoChar = @(
-        [Char](0x00A7)#, # The original!
-        [Char](0x2699),
-        [Char](0x2B55),
-        [Char]::ConvertFromUtf32('0x1F527'),
-        [Char]::ConvertFromUtf32('0x1F506'),
-        [Char]::ConvertFromUtf32('0x1F534'),
-        [Char]::ConvertFromUtf32('0x1F535'),
-        [Char](0x26AB),
-        [Char](0x26AA),
-        [Char]::ConvertFromUtf32('0x1F536'),
-        [Char]::ConvertFromUtf32('0x1F537'),
-        [Char]::ConvertFromUtf32('0x1F4A0'),
-        [Char]::ConvertFromUtf32('0x1F518')
-    )
-    Get-Random -InputObject $decoChar
-}
-
-$escChar = [Char](0x1B)
-$gitChar = [Char](0xE0A0)
-$rightArrowFull = [Char](0xE0B0)
-$rightArrowHalf = [Char](0xE0B1)
-
-# Colors / Font Modifiers
-$ansiReset = "$escChar[0m"
-$ansiSlowBlink = "$escChar[5m"
-
-$ansiColorBlack = "0;0;0"
-$ansiColorWhite = "255;255;255"
-$ansiColorDarkRed = "136;20;2"
-$ansiColorDarkGreen = "3;82;0"
-
-$ansiColorPurple = "70;23;145"
-$ansiColorGitOrange = "241;80;47"
-$ansiColorBlue = "70;167;252"
-$ansiColorLightPurple = "201;26;204"
-
-$ansiDecorator = "$escChar[38;2;$ansiColorBlack;48;2;$ansiColorBlue`m"
-$ansiDecoratorToComputerNameTrans = "$escChar[38;2;$ansiColorBlue;48;2;$ansiColorPurple`m"
-$ansiComputerName = "$escChar[38;2;$ansiColorWhite;48;2;$ansiColorPurple`m"
-$ansiComputerNameDirectoryTrans = "$escChar[38;2;$ansiColorPurple;48;2;$ansiColorLightPurple`m"
-$ansiDirectory = "$escChar[38;2;$ansiColorBlack;48;2;$ansiColorLightPurple`m"
-$ansiDirectoryEndPrompt = "$escChar[38;2;$ansiColorLightPurple`m"
-
-$ansiDirectoryToGitTans = "$escChar[38;2;$ansiColorLightPurple;48;2;$ansiColorGitOrange`m"
-$ansiGit = "$escChar[38;2;$ansiColorBlack;48;2;$ansiColorGitOrange`m"
-$ansiGitUnStaged = "$escChar[38;2;$ansiColorDarkRed;48;2;$ansiColorGitOrange`m"
-$ansiGitStaged = "$escChar[38;2;$ansiColorDarkGreen;48;2;$ansiColorGitOrange`m"
-$ansiGitEndPrompt = "$escChar[38;2;$ansiColorGitOrange`m"
-
 function prompt {
     # Microsoft Docs ANSI ESC sequences (https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences)
+    # $lastRun needs to be first otherwise it will interpret successful prompt commands as good and always make the circle green.
+    if ($?) {
+        $lastRun = "💠"
+    } else {
+        $lastRun = "❌"
+    }
+
+    # Characters / Keys
+    $gitChar = [Char](0xE0A0)
+    $rightArrowFull = [Char](0xE0B0)
+    $rightArrowHalf = [Char](0xE0B1)
+    
+    # Colors / Font Modifiers
+    $ansiReset = "`e[0m"
+    $ansiSlowBlink = "`e[5m"
+    
+    $ansiColorBlack = "0;0;0"
+    $ansiColorWhite = "255;255;255"
+    $ansiColorDarkRed = "136;20;2"
+    $ansiColorDarkGreen = "3;82;0"
+    
+    $ansiColorPurple = "70;23;145"
+    $ansiColorGitOrange = "241;80;47"
+    $ansiColorBlue = "70;167;252"
+    $ansiColorLightPurple = "201;26;204"
+    
+    $ansiDecorator = "`e[38;2;$ansiColorBlack;48;2;$ansiColorBlue`m"
+    $ansiDecoratorToComputerNameTrans = "`e[38;2;$ansiColorBlue;48;2;$ansiColorPurple`m"
+    $ansiComputerName = "`e[38;2;$ansiColorWhite;48;2;$ansiColorPurple`m"
+    $ansiComputerNameDirectoryTrans = "`e[38;2;$ansiColorPurple;48;2;$ansiColorLightPurple`m"
+    $ansiDirectory = "`e[38;2;$ansiColorBlack;48;2;$ansiColorLightPurple`m"
+    $ansiDirectoryEndPrompt = "`e[38;2;$ansiColorLightPurple`m"
+    
+    $ansiDirectoryToGitTans = "`e[38;2;$ansiColorLightPurple;48;2;$ansiColorGitOrange`m"
+    $ansiGit = "`e[38;2;$ansiColorBlack;48;2;$ansiColorGitOrange`m"
+    $ansiGitUnStaged = "`e[38;2;$ansiColorDarkRed;48;2;$ansiColorGitOrange`m"
+    $ansiGitStaged = "`e[38;2;$ansiColorDarkGreen;48;2;$ansiColorGitOrange`m"
+    $ansiGitEndPrompt = "`e[38;2;$ansiColorGitOrange`m"
+    
 
     # Get current path parent and leaf.
     $currentDirectoryLeaf = Split-Path (Get-Location) -Leaf
@@ -84,8 +69,7 @@ function prompt {
     if ($IsWindows) {
         $currentIdentity = New-Object -TypeName System.Security.Principal.WindowsPrincipal -ArgumentList @([System.Security.Principal.WindowsIdentity]::GetCurrent())
         $isAdminOrRoot = $currentIdentity.IsInRole([System.Security.Principal.WindowsBuiltInRole]"Administrator")
-    }
-    else {
+    } else {
         $isAdminOrRoot = (
             ((whoami) -eq 'root') -or
             ((groups) -split ' ' -contains 'root') # You could optionally add wheel if you want.
@@ -96,16 +80,13 @@ function prompt {
     if ($IsWindows) {
         if ($isAdminOrRoot) {
             $currentUser = "$env:USERNAME [ADMIN]"
-        }
-        else {
+        } else {
             $currentUser = "$env:USERNAME [USER]"
         }
-    }
-    else {
+    } else {
         if ($isAdminOrRoot) {
             $currentUser = "$env:USER [ROOT]"
-        }
-        else {
+        } else {
             $currentUser = "$env:USER [USER]"
         }
     }
@@ -113,8 +94,11 @@ function prompt {
     # Prompt (Right Side, No left side in this case).  Create a list of scriptblocks.
     # While it does add a tiny bit of complexity, it makes prompt parts modular and swappable over strining Write-Host in a specific order.
     [System.Collections.Generic.List[ScriptBlock]]$promptPartsAll = @(
-        # { "$ansiDecorator"; "$decoChar"; "$ansiReset" }
-        { "$ansiDecorator"; "$(Get-RandomPromptChar)"; "$ansiReset" }
+        { 
+            "$ansiDecorator"
+            "$lastRun"
+            "$ansiReset"
+        }
         { "$ansiDecoratorToComputerNameTrans"; "$rightArrowFull"; "$ansiReset" }
         { "$ansiComputerName"; "$([System.Net.Dns]::GetHostName())"; "$ansiReset" }
         { "$ansiComputerNameDirectoryTrans"; "$rightArrowFull"; "$ansiReset" }
@@ -232,9 +216,15 @@ else {
     Write-Warning -Message "Go Language not installed, skipping."
 }
 ############################
-#endregion
 
-#region Custom Alias'
+#Ruby Stuff
+############################
+if ((ruby -v) -and (gem -v)) {
+    $env:PATH = $env:PATH += (":{0}/bin" -f (ruby -r rubygems -e 'puts Gem.user_dir'))
+}
+############################
+
+#region Custom Alias
 ############################
 
 New-Alias -Name ih -Value Invoke-History -Force
@@ -266,7 +256,7 @@ if ($IsWindows) {
         New-Alias -Name vim -Value 'C:\Program Files\Vim\vim81\vim.exe' -Force
     }
 } else {
-    New-Alias -Name ls -Value 'ls --color=auto'
+    # New-Alias -Name ls -Value 'ls --color=auto'
 }
 ############################
 #endregion
